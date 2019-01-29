3b6ebdcfe1286915dd506af5b2314ec1265874c2
Travis CI <travis@travis-ci.org>
2019-01-30 03:09:04 +0800

    Travis build:

--- git_push.sh
+++ git_push.sh
@@ -9,8 +9,8 @@ commit_website_files() {
 #  git checkout -b gh-pages
 #  git add ./ -A
   git status
-  git add gcr-complete-images gcr-complete-images
-  git commit -a --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git add gcr-complete-images gcr-complete-tasks
+  git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
@@ -18,7 +18,7 @@ upload_files() {
   [ -n "`git remote | grep $REMOTE_REF`" ] && git remote remove ${REMOTE_REF}
   git remote add ${REMOTE_REF} https://${GH_TOKEN}@github.com/blademainer/google_containers_mirror.git
 #  git push --quiet --set-upstream tasks master
-    git pull && git push --set-upstream ${REMOTE_REF} master
+  git pull ${REMOTE_REF} master && git push --set-upstream ${REMOTE_REF} master
 }
 
 setup_git
