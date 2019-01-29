e8157903c3a51bc89f960a95fdf4f59fcd8d6d28
blademainer <blademainer@gmail.com>
2019-01-30 04:05:36 +0800

    Update git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config user.email "travis@travis-ci.org"
+  git config user.name "Travis CI"
 }
 
 upload_files() {
